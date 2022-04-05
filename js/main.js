8fada3421619a0e6a802423fa883753e543dca79
Jamoliddin610 <najmiddinovjamoliddin571@gmail.com>
2022-04-05 14:19:16 +0500

    jamoliddin  qo'lidan kegancha tugadi ,qogani allohdan

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 instaHistory = [{
         id: 1,
-        media: ['https://picsum.photos/id/247/200/300', 'https://picsum.photos/id/267/200/300', 'https://picsum.photos/id/257/200/300'],
+        media: ['https://picsum.photos/id/17/200/300', 'https://picsum.photos/id/267/200/300', 'https://picsum.photos/id/257/200/300'],
         logo: './media/javohir.jpg',
         name: "javohir.coding",
         time: '17h',
@@ -8,7 +8,7 @@ instaHistory = [{
     },
     {
         id: 2,
-        media: ['./media/ajr-video.mp4'],
+        media: ['./media/ajr-img-video.jpg','./media/ajr-video.mp4'],
         logo: './media/ajr.jpg',
         name: "ajr_collection",
         time: '3h',
@@ -17,7 +17,7 @@ instaHistory = [{
     {
         id: 3,
 
-        media: ['https://picsum.photos/id/217/200/300', 'https://picsum.photos/id/27/200/300'],
+        media: ['https://picsum.photos/id/797/200/300', 'https://picsum.photos/id/27/200/300'],
         logo: './media/muhammad.jpg',
         name: "muhammadali_eshonqulov",
         time: '21h',
@@ -26,7 +26,7 @@ instaHistory = [{
     {
         id: 4,
 
-        media: ['https://picsum.photos/id/277/200/300'],
+        media: ['https://picsum.photos/id/878/200/300'],
         logo: './media/fayzulla.jpg',
         name: "feruzallaev",
         time: '9h',
@@ -35,7 +35,7 @@ instaHistory = [{
     {
         id: 5,
 
-        media: ['./media/abdukarim.mp4'],
+        media: ['./media/img-video.jpg', './media/abdukarim.mp4'],
         logo: './media/abdukarim.jpg',
         name: "abdukarimmirzayev",
         time: '2h',
@@ -48,12 +48,12 @@ let cont = document.querySelector(".row")
 instaHistory.forEach((elem) => {
     let wrapper = document.createElement('div')
     wrapper.className = 'history__wrapper'
-    wrapper.innerHTML = `<div class="history__line d-flex mb-3">
+    wrapper.innerHTML = `<div class="history__line d-none mb-3">
           <div class="line col-4"><span class="compl"></span></div>
           <div class="line col-4"><span class="compl"></span></div>
           <div class="line col-4"><span class="compl"></span></div>
         </div>
-        <div class="history__header header d-flex align-items-center justify-content-between">
+        <div class="history__header header d-none align-items-center justify-content-between">
           <div class="header__about d-flex align-items-center">
             <div class="header__img-wrapper me-2">
               <img src="${elem.logo}" alt="logo">
@@ -73,34 +73,71 @@ instaHistory.forEach((elem) => {
         <div class="history__media">
         <img src="${elem.media[0]}" alt="">
         </div>
-        <div class="history__footer footer d-flex align-items-center">
+        <div class="history__avatar">
+        <div class="history__avatar-img-wrapper">
+        <img src="${elem.logo}" alt="logo">
+        
+        </div>
+        <p class="history__name ">
+              ${elem.name}
+            </p>
+            <span class="history__time">${elem.time}</span></div>
+        <div class="history__footer footer d-none align-items-center">
           <input type="text" class="footer__input me-3" placeholder="Reply to ${elem.name}...">
           <i class="bx bx-send"></i>  
-        </div>`
+        </div>
+        `
     cont.appendChild(wrapper)
-    let media = document.querySelectorAll(".history__media")
-    media.forEach((item, ind) => {
-        if (elem.type == 'video') {
-            if (ind + 1 == elem.id) {
-                item.innerHTML = `<video width="100%" height="100%"  src="${elem.media}" autoplay type="video/mp4"></video>`
-            }
-        }
-    });
+
 })
-let wrap = document.querySelectorAll('.history__wrapper')
-instaHistory.forEach((elem)=>{
-    wrap.forEach((item,idx)=>{
-        let num = 0
-        item.addEventListener('click',()=>{
-            if (idx<num) {
-                cont.style.transform = 'translate(410px)'
-                num=idx
+    let wrap = document.querySelectorAll('.history__wrapper')
+    wrap[0].classList.add('active')
+    wrap.forEach((item, idx) => {
+        item.addEventListener('click', () => {
+        
+            if (idx == 1) {
+                cont.style.transform = `translate(410px)`
+                wrap[1].classList.add('active')
+                wrap[2].classList.remove('active')
+                wrap[3].classList.remove('active')
+                wrap[4].classList.remove('active')
+                wrap[0].classList.remove('active')
+                
             }
-            else if (idx>num) {
-                cont.style.transform = 'translate(-410px)'
-                num=idx
+            if (idx == 2) {
+                cont.style.transform = `translate(0px)`
+                wrap[2].classList.add('active')
+                wrap[1].classList.remove('active')
+                wrap[3].classList.remove('active')
+                wrap[4].classList.remove('active')
+                wrap[0].classList.remove('active')
+            }
+            if (idx == 0) {
+                cont.style.transform = `translate(820px)`
+                wrap[0].classList.add('active')
+                wrap[1].classList.remove('active')
+                wrap[2].classList.remove('active')
+                wrap[4].classList.remove('active')
+                wrap[3].classList.remove('active')
+            }
+            if (idx == 3) {
+                cont.style.transform = `translate(-410px)`
+                wrap[3].classList.add('active')
+                wrap[1].classList.remove('active')
+                wrap[4].classList.remove('active')
+                wrap[2].classList.remove('active')
+                wrap[0].classList.remove('active')
+            }
+            if (idx == 4) {
+                cont.style.transform = `translate(-820px)`
+                wrap[4].classList.add('active')
+                wrap[1].classList.remove('active')
+                wrap[3].classList.remove('active')
+                wrap[2].classList.remove('active')
+                wrap[0].classList.remove('active')
             }
         })
     })
-})
+
+
 
